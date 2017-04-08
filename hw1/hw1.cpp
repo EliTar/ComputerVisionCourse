4451b84722b79d6c6ccffde7e9964e9b6c0d734f
Andrey Leshenko <andrey.leshenko@gmail.com>
2017-04-08 18:23:11 +0300

    Finish code review

--- hw1/hw1.cpp
+++ hw1/hw1.cpp
@@ -47,7 +47,7 @@ using cv::Mat3f;
 const int patchRadius = 3;
 const int sideLength = 2 * patchRadius + 1;
 
-void printTimeSinceLastCall(const char* message)
+void PrintTimeSinceLastCall(const char* message)
 {
 	static s64 freq = static_cast<int>(cv::getTickFrequency());
 	static s64 last = cv::getTickCount();
@@ -396,7 +396,7 @@ Vec3b ConvertLabelToColor(int label)
 
 Mat FragmentToColorDistVisualization(Mat image, Mat imageLabels, vector<vector<double>> distances, int label)
 {
-    Mat vizImage = Mat::zeros(cv::Size(image.cols, image.rows), CV_8UC3);
+    Mat vizImage = Mat::zeros(image.size(), CV_8UC3);
 
     for(int i = 0; i < imageLabels.rows; i++)
     {
@@ -412,35 +412,13 @@ Mat FragmentToColorDistVisualization(Mat image, Mat imageLabels, vector<vector<d
         }
     }
 
-    // vector<bool> imageLabels(size.)
-
-    // for(int i = 0; i < imageLabels.rows; i++)
-    // {
-    //     for(int j = 0; j < imageLabels.cols; j++)
-    //     {
-    //         string text = (string)dist;
-    //         int fontFace = FONT_HERSHEY_SCRIPT_SIMPLEX;
-    //         double fontScale = 2;
-    //         int thickness = 3;
-    //         cv::Point textOrg(10, 130);
-    //         cv::putText(img, text, textOrg, fontFace, fontScale, Scalar::all(255), thickness,8);
-
-    //         Vec3b pixelColor = image.at<Vec3b>(i , j);
-    //         double dist = distances[imageLabels.at<int>(i , j)][label];
-    //         uchar a = round(pixelColor[0] * dist);
-    //         uchar b = round(pixelColor[1] * dist);
-    //         uchar c = round(pixelColor[2] * dist);
-
-    //         vizImage.at<Vec3b>(i , j) = Vec3b(a, b, c);
-    //     }
-    // }
-
     return vizImage;
 }
 
 Mat PaintLabelsTrainImage(Mat imageLabels)
 {
     imageLabels.convertTo(imageLabels, CV_32S);
+
     Mat newImage = Mat::zeros(cv::Size(imageLabels.cols, imageLabels.rows), CV_8UC3);
     for(int i = 0; i < imageLabels.rows; i++)
     {
@@ -493,15 +471,6 @@ Mat PaintInAverageColor(Mat image, Mat imageLabels)
         avgPixels[i][1] = sumB[i] / howMany[i];
         avgPixels[i][2] = sumC[i] / howMany[i];
     }
-
-    // vector<Vec3b> pixelAvg(maxLabel + 1);
-
-    // for(int i = 0; i < maxLabel + 1; i++)
-    // {
-    //     Vec3b sum = std::accumulate(pixelValues[i].begin(), pixelValues[i].end(), 0);
-    //     // Vec3b avg = sum / pixelValues[i].size();
-    //     // pixelAvg[i] = avg;
-    // }
 
     Mat newImage = Mat::zeros(cv::Size(image.cols, image.rows), CV_8UC3);
 
@@ -603,9 +572,6 @@ vector<vector<Rect>> RandomPatchesForEachLabel(Mat image, Mat imageLabels, bool 
 	return patches;
 }
 
-// TODO: Check this function!!! likes to give exeptions. maybe its Vec3b?
-// Fixed. Probably the problem was with acssesing outside the matrix...
-
 Mat VisualizePatches(Mat image, vector<vector<Rect>> patches, int patchLabel = -1)
 {
     Mat vizPatch = Mat::zeros(cv::Size(image.cols, image.rows), CV_8UC3);
@@ -616,14 +582,7 @@ Mat VisualizePatches(Mat image, vector<vector<Rect>> patches, int patchLabel = -
         {
             for(Rect rec : patches[i])
             {
-                for(int i = rec.x; i < rec.x + sideLength; i++)
-                {
-                    for(int j = rec.y; j < rec.y + sideLength; j++)
-                    {
-                        if( i < image.rows && j < image.cols )
-                            vizPatch.at<cv::Vec3b>(i,j) = image.at<cv::Vec3b>(i,j);
-                    }
-                }
+				(Mat{image, rec}).copyTo(Mat{vizPatch, rec});
             }
         }
     }
@@ -691,52 +650,19 @@ Mat DrawBorderFromLabels(Mat image, Mat imageLabels)
     return newImage;
 }
 
-Mat SubtructFregmentAverageColor(Mat image, Mat imageLabels)
+void DecreaseImageContrast(Mat &image, int by = 2)
 {
-    imageLabels.convertTo(imageLabels, CV_32S);
-    Mat newImage = Mat::zeros(cv::Size(image.cols, image.rows), CV_8UC3);
+	cv::cvtColor(image, image, cv::COLOR_BGR2HSV);
 
-    const int   imageRows = image.rows,
-                imageCols = image.cols;
-    double      maxLabel,
-                minLabel;
+	for (int r = 0; r < image.rows; r++) {
+		for (int c = 0; c < image.cols; c++) {
+			Vec3b color = image.at<Vec3b>(r, c);
+			color[2] = 128 + (color[2] - 128) / by;
+			image.at<Vec3b>(r, c) = color;
+		}
+	}
 
-    minMaxLoc(imageLabels, &minLabel, &maxLabel);
-
-    vector<vector<Point>> pixelsLabeledPoints(maxLabel + 1);
-    vector<vector<int>> pixelsLabeledValues(maxLabel + 1);
-
-    for(int i = 0; i < imageRows; i++)
-    {
-        for(int j = 0; j < imageCols; j++)
-        {
-            pixelsLabeledPoints[imageLabels.at<int>(i , j)].push_back(Point(i, j));
-            pixelsLabeledValues[imageLabels.at<int>(i , j)].push_back(image.at<Vec3b>(i, j)[0]);
-            pixelsLabeledValues[imageLabels.at<int>(i , j)].push_back(image.at<Vec3b>(i, j)[1]);
-            pixelsLabeledValues[imageLabels.at<int>(i , j)].push_back(image.at<Vec3b>(i, j)[2]);
-        }
-    }
-
-    for(int i = 0; i < (int)pixelsLabeledPoints.size(); i++)
-    {
-        int fregmentAverage = (int) (accumulate( pixelsLabeledValues[i].begin(), pixelsLabeledValues[i].end(), 0.0) / (pixelsLabeledValues[i].size() * 3) );
-        for(Point p : pixelsLabeledPoints[i])
-        {
-            Vec3b currentColor = image.at<Vec3b>(p.x , p.y);
-            int a = currentColor[0] - fregmentAverage;
-            int b = currentColor[1] - fregmentAverage;
-            int c = currentColor[2] - fregmentAverage;
-            if(a < 0)
-                a = 0;
-            if(b < 0)
-                b = 0;
-            if(c < 0)
-                c = 0;
-            newImage.at<Vec3b>(p.x , p.y) = Vec3b(a, b, c);
-        }
-    }
-
-    return newImage;
+	cv::cvtColor(image, image, cv::COLOR_HSV2BGR);
 }
 
 void Usage()
@@ -770,21 +696,6 @@ Mat LoadImageWithSomeExtension(string basePath, int flags = cv::IMREAD_COLOR)
 
 	return Mat{};
 
-}
-
-void DecreaseImageContrast(Mat &image, int by = 2)
-{
-	cv::cvtColor(image, image, cv::COLOR_BGR2HSV);
-
-	for (int r = 0; r < image.rows; r++) {
-		for (int c = 0; c < image.cols; c++) {
-			Vec3b color = image.at<Vec3b>(r, c);
-			color[2] = 128 + (color[2] - 128) / by;
-			image.at<Vec3b>(r, c) = color;
-		}
-	}
-
-	cv::cvtColor(image, image, cv::COLOR_HSV2BGR);
 }
 
 int main(int argc, char *argv[])
@@ -863,7 +774,7 @@ int main(int argc, char *argv[])
 			50,
 			0.005);
 
-    printTimeSinceLastCall("Generate Super Pixels");
+    PrintTimeSinceLastCall("Generate Super Pixels");
 
 	UTILITY
 	{
@@ -880,7 +791,7 @@ int main(int argc, char *argv[])
 
     vector<vector<Rect>> testFragmentPatches = RandomPatchesForEachLabel(testImage, testLabels);
 
-    printTimeSinceLastCall("Random patches test");
+    PrintTimeSinceLastCall("Random patches test");
 
 	UTILITY
 	{
@@ -894,7 +805,7 @@ int main(int argc, char *argv[])
 	const bool innerPatchesOnly = true;
     vector<vector<Rect>> trainPatches = RandomPatchesForEachLabel(trainImage, trainLabels, innerPatchesOnly);
 
-    printTimeSinceLastCall("Random patches train");
+    PrintTimeSinceLastCall("Random patches train");
 
 	UTILITY
 	{
@@ -952,7 +863,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    printTimeSinceLastCall("Calculate patch distances");
+    PrintTimeSinceLastCall("Calculate patch distances");
 
     // Choose the median value of all minimum distances.
     // A robust voting scheme as explaind in the paper.
@@ -974,7 +885,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    printTimeSinceLastCall("Calculate fragment distances");
+    PrintTimeSinceLastCall("Calculate fragment distances");
 
     // We normalize each fragment distace to the range of [0, 1]
     // as asked in the assigment.
@@ -1009,7 +920,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    printTimeSinceLastCall("Normalize Distances");
+    PrintTimeSinceLastCall("Normalize Distances");
 
     // Normalize each fragment distance.
     // After a discussion with Hagit and a test we've made,
@@ -1036,7 +947,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    printTimeSinceLastCall("Normalize each fregment distance");
+    PrintTimeSinceLastCall("Normalize each fregment distance");
 
     UTILITY
     {
@@ -1092,7 +1003,7 @@ int main(int argc, char *argv[])
         // This sets pixels that are equal to 255.
 
         string grabCutString = "Grab Cut " + std::to_string(labelNumber);
-        printTimeSinceLastCall(grabCutString.c_str());
+        PrintTimeSinceLastCall(grabCutString.c_str());
 
         foregroundImages[labelNumber] = Mat{avgColoredImage.size(), CV_8UC3, Scalar{255,255,255}};
         testImage.copyTo(foregroundImages[labelNumber], grabCutMask);
@@ -1150,7 +1061,7 @@ int main(int argc, char *argv[])
         }
     }
 
-    printTimeSinceLastCall("Final Labeling");
+    PrintTimeSinceLastCall("Final Labeling");
 
     for(int i = 0; i < (int)trainPatches.size(); i++)
     {
